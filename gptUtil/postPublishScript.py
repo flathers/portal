f54ced5c731e703d36d9396a3498feaf054d8ba7
flathers <flathers+gh@gmail.com>
2016-03-16 23:56:17 -0700

    Updated postPublishScript

--- gptUtil/postPublishScript.py
+++ gptUtil/postPublishScript.py
@@ -5,6 +5,7 @@ from subprocess import Popen, PIPE
 import shutil
 import smtplib
 import sys
+import os
 
 uuid = sys.argv[1]
 basePath = '/preprod-datastore/uploads/'
@@ -16,19 +17,56 @@ datastorePath = '/datastore/published/'
 #
 #Clamscan the dataset path
 #
+exit_code = 0
+if (os.path.isdir('dsPath')):
+  cmd = "/usr/bin/clamscan " + dsPath
+  process = Popen(shlex.split(cmd), stdout=PIPE)
+  process.communicate()
+  exit_code = process.wait()
+#print "~~~~~exit code " + str(exit_code)
 
-cmd = "/usr/bin/clamscan " + dsPath
-process = Popen(shlex.split(cmd), stdout=PIPE)
-process.communicate()
-exit_code = process.wait()
 
 
+#
+#Send the notification email
+#
+
+sender = 'portal@northwestknowledge.net'
+receivers = ['publish@northwestknowledge.net']
+
+if (exit_code == 0):
+  message = """From: NKN Geoportal <portal@northwestknowledge.net>
+To: NKN Publisher Group <publish@northwestknowledge.net>
+Subject: New data posted for publication
+
+Hi, NKN data publishers.  A new dataset has been approved for publication
+in the geoportal interface. 
+"""
+else:
+  message = """From: NKN Geoportal <portal@northwestknowledge.net>
+To: NKN Publisher Group <publish@northwestknowledge.net>
+Subject: New data posted for publication [virus]
+
+Hi, NKN data publishers.  A new dataset has been posted for publication
+in the geoportal interface, but the data were infected with a virus.
+Please take a look.
+"""
+
+
+try:
+   smtpObj = smtplib.SMTP('localhost')
+   smtpObj.sendmail(sender, receivers, message)
+   print "Successfully sent email"
+except SMTPException:
+   print "Error: unable to send email"
 
 #
 #If the files were infected, send a notification email
 #
 
 if (exit_code != 0):
+  #A virus was found, stop processing
+
   sender = 'portal@northwestknowledge.net'
   receivers = ['publish@northwestknowledge.net']
   message = """From: NKN Geoportal <portal@northwestknowledge.net>
@@ -50,8 +88,11 @@ take a look.
 
 
 
+
 #
-#If the files are clean, move the files into production
+#If the files are clean, move them into production
 #
 if (exit_code == 0):
-  shutil.move(dsPath, datastorePath + uuid)
+  if (os.path.isdir('dsPath')):
+    shutil.move(dsPath, datastorePath + uuid)
+
