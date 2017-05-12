a97c0b74b30e478eca23a3cbdb83af066dd55e20
flathers <flathers+gh@gmail.com>
2017-05-11 18:05:28 -0700

    collections

--- search/search.js
+++ search/search.js
@@ -1,5 +1,6 @@
 fromVal = 0;
 sizeVal = 5;
+pageVal = 0;
 totalRecords = 0;
 next = 1;
 prev = -1;
@@ -7,6 +8,9 @@ prev = -1;
 $(document).ready(function(){ init(); });
 
 function init() {
+  $("#prevPage").attr('hidden', true);
+  $("#nextPage").attr('hidden', true);
+
   $("#search").click(function() {
     $("#search").attr('disabled', true);
     searchKey = $("#searchText").val();
@@ -40,20 +44,52 @@ function init() {
     searchKey = $("#searchText").val();
     page(searchKey, prev);
   });
-
-
 }
+
 
 function doSearch(key) {
   $("#searchResultContainer").html("");
-  url = "/_search/"
-  $.getJSON(url, {
-      default_operator: "AND",
-      from: fromVal,
-      q: key,
-      size: sizeVal,
-      _source: "mdXmlPath"
-    },
+
+  /*
+  This query selects only records where collection is null,
+  which screens out granules; matches the search key in all
+  fields; and provides a scoring bonus to records for which
+  the title includes the search key.
+  */
+
+  query = JSON.stringify({
+    size: sizeVal,
+    from: fromVal,
+    query: {
+      bool: {
+        must_not: {
+          exists: {
+            field: "collection"
+          }
+        },
+        must: {
+          match: {
+            _all: {
+              query: key,
+              operator: "and"
+            }
+          }
+        },
+        should: {
+          match: {
+            title: {
+              query: key,
+              operator: "and",
+              boost: 20
+            }
+          }
+        }
+      }
+    }
+  });
+
+  url = "/search/"
+  $.post(url, query,
     function(data) {
       baseUrl = '/portal/renderMetadata/php/render.php?xml=';
       totalRecords = parseInt(data.hits.total);
@@ -63,31 +99,38 @@ function doSearch(key) {
           $("#searchResultContainer").append(data + '<hr>');
         });
       });
+      page(key, 0);
     }
   );
 }
 
+
 function page(key, direction) {
-  newFromVal = fromVal;
-  newFromVal += sizeVal * direction;
+  if (direction === next)
+    pageVal += 1;
+  else if (direction === prev)
+    pageVal -= 1;
 
-  if(newFromVal > totalRecords)
-    newFromVal = fromVal;
-  fromVal = newFromVal;
+  if (pageVal <= 0) {
+    pageVal = 0;
+    $("#prevPage").attr('hidden', true);
+  }
+  else
+    $("#prevPage").attr('hidden', false);
 
-  if(fromVal + sizeVal >= totalRecords)
+  if (pageVal >= Math.floor(totalRecords / sizeVal)) {
+    pageVal = Math.floor(totalRecords / sizeVal);
     $("#nextPage").attr('hidden', true);
+  }
   else
     $("#nextPage").attr('hidden', false);
 
-  if(fromVal >= sizeVal)
-    $("#prevPage").attr('hidden', false);
+  //If direction==0, this was a search, not a page turn
+  //so all we need to do is check the next/previous visibility
+  if (direction == 0)
+    return;
 
-  if(fromVal <= 0) {
-    fromVal = 0;
-    $("#prevPage").attr('hidden', true);
-  }
-  $("#currentPage").html('Current Page: ' + ((fromVal/sizeVal)+1));
+  fromVal = sizeVal * pageVal;
+  $("#currentPage").html('Current Page: ' + (pageVal+1));
   doSearch(key);
-//  $("#search").click();
 }
